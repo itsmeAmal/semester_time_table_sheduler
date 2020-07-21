b8b032b84a99388c7982d0d9a141cc32ae9a4260
Amal <wickramarathna.amal@gmail.com>
2020-07-21 12:06:29 +0530

    update holidayDaoImpl class

--- src/com/ttms/daoimpl/holidayDaoImpl.java
+++ src/com/ttms/daoimpl/holidayDaoImpl.java
@@ -37,27 +37,39 @@ public class holidayDaoImpl implements holidayDao {
 
     @Override
     public boolean updateHoliday(holiday holiday) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = DatabaseConnection.getDatabaseConnection();
+        PreparedStatement ps = con.prepareStatement("update holiday set holiday_date_from=?, holiday_date_to=?, holiday_detail=? where holiday_id=?");
+        ps.setDate(1, holiday.getFromDate());
+        ps.setDate(2, holiday.getToDate());
+        ps.setString(3, holiday.getDetail());
+        ps.setInt(4, holiday.getId());
+        ps.executeUpdate();
+        ps.close();
+        return true;
     }
 
     @Override
     public ResultSet getAllHolidays() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new commonDaoImpl().getAllRecords(selectQuery);
     }
 
     @Override
     public ResultSet getHolidayByOneAttribute(String attribute, String condition, String value) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new commonDaoImpl().getResultByAttribute(selectQuery, attribute, condition, value);
     }
 
     @Override
     public ResultSet getHolidayByMoreAttributes(ArrayList<String[]> attributeConditionValueList, String operator) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new commonDaoImpl().getResultByAttributesWithJoinOperator(selectQuery, attributeConditionValueList, operator);
     }
 
     @Override
     public boolean deleteHoliday(int holidayId) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = DatabaseConnection.getDatabaseConnection();
+        PreparedStatement ps = con.prepareStatement("delete from holiday where holiday_id=?");
+        ps.setInt(1, holidayId);
+        ps.executeUpdate();
+        ps.close();
+        return true;
     }
-
 }

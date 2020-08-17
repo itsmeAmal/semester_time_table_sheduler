85095c5773e6cfcb93b443f528a7d098924be50e
Amal <wickramarathna.amal@gmail.com>
2020-08-18 04:52:27 +0530

    add delivery plan details model and dao classes

---
+++ src/com/ttms/model/deliveryPlanDetails.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ttms.model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author personal
+ */
+public class deliveryPlanDetails {
+
+    private int id;
+    private int deliveryPlanId;
+    private Date timeTableDate;
+    private String timeTableTime;
+    private int timeOrderNo;
+    private int status;
+    private String remark;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the deliveryPlanId
+     */
+    public int getDeliveryPlanId() {
+        return deliveryPlanId;
+    }
+
+    /**
+     * @param deliveryPlanId the deliveryPlanId to set
+     */
+    public void setDeliveryPlanId(int deliveryPlanId) {
+        this.deliveryPlanId = deliveryPlanId;
+    }
+
+    /**
+     * @return the timeTableDate
+     */
+    public Date getTimeTableDate() {
+        return timeTableDate;
+    }
+
+    /**
+     * @param timeTableDate the timeTableDate to set
+     */
+    public void setTimeTableDate(Date timeTableDate) {
+        this.timeTableDate = timeTableDate;
+    }
+
+    /**
+     * @return the timeTableTime
+     */
+    public String getTimeTableTime() {
+        return timeTableTime;
+    }
+
+    /**
+     * @param timeTableTime the timeTableTime to set
+     */
+    public void setTimeTableTime(String timeTableTime) {
+        this.timeTableTime = timeTableTime;
+    }
+
+    /**
+     * @return the timeOrderNo
+     */
+    public int getTimeOrderNo() {
+        return timeOrderNo;
+    }
+
+    /**
+     * @param timeOrderNo the timeOrderNo to set
+     */
+    public void setTimeOrderNo(int timeOrderNo) {
+        this.timeOrderNo = timeOrderNo;
+    }
+
+    /**
+     * @return the status
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the remark
+     */
+    public String getRemark() {
+        return remark;
+    }
+
+    /**
+     * @param remark the remark to set
+     */
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+}

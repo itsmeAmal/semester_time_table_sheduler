81e74ca289b9ecd7dc019c0388fa9c851c86ea02
Amal <wickramarathna.amal@gmail.com>
2020-08-30 12:33:51 +0530

    change/update/add new delivery plan detail model class

--- src/com/ttms/model/deliveryPlanDetails.java
+++ src/com/ttms/model/deliveryPlanDetails.java
@@ -13,14 +13,30 @@ import java.sql.Date;
  */
 public class deliveryPlanDetails {
 
+    //delivery_plan_details_id, delivery_plan_details_delivery_plan_id,
+    //delivery_plan_details_date, delivery_plan_details_time, 
+    //delivery_plan_details_time_order_no, delivery_plan_details_status, 
+    //delivery_plan_details_remark, delivery_plan_details_day,
+    //delivery_plan_details_level, delivery_plan_details_module_name, 
+    //delivery_plan_details_module_code, delivery_plan_details_type, 
+    //delivery_plan_details_lecturer_name, delivery_plan_details_room_name, 
+    //delivery_plan_details_course_name, delivery_plan_details_group_name
     private int id;
-    private int deliveryPlanId;
-    private Date timeTableDate;
-    private String timeTableTime;
-    private int timeOrderNo;
+    private int deliveryPlanid;
+    private Date date;
+    private String timeString;
+    private int timeOrder;
     private int status;
     private String remark;
     private String day;
+    private String level;
+    private String modueName;
+    private String moduleCode;
+    private String type;
+    private String lecturerName;
+    private String roomName;
+    private String courseName;
+    private String groupName;
 
     /**
      * @return the id
@@ -37,59 +53,59 @@ public class deliveryPlanDetails {
     }
 
     /**
-     * @return the deliveryPlanId
+     * @return the deliveryPlanid
      */
-    public int getDeliveryPlanId() {
-        return deliveryPlanId;
+    public int getDeliveryPlanid() {
+        return deliveryPlanid;
     }
 
     /**
-     * @param deliveryPlanId the deliveryPlanId to set
+     * @param deliveryPlanid the deliveryPlanid to set
      */
-    public void setDeliveryPlanId(int deliveryPlanId) {
-        this.deliveryPlanId = deliveryPlanId;
+    public void setDeliveryPlanid(int deliveryPlanid) {
+        this.deliveryPlanid = deliveryPlanid;
     }
 
     /**
-     * @return the timeTableDate
+     * @return the date
      */
-    public Date getTimeTableDate() {
-        return timeTableDate;
+    public Date getDate() {
+        return date;
     }
 
     /**
-     * @param timeTableDate the timeTableDate to set
+     * @param date the date to set
      */
-    public void setTimeTableDate(Date timeTableDate) {
-        this.timeTableDate = timeTableDate;
+    public void setDate(Date date) {
+        this.date = date;
     }
 
     /**
-     * @return the timeTableTime
+     * @return the timeString
      */
-    public String getTimeTableTime() {
-        return timeTableTime;
+    public String getTimeString() {
+        return timeString;
     }
 
     /**
-     * @param timeTableTime the timeTableTime to set
+     * @param timeString the timeString to set
      */
-    public void setTimeTableTime(String timeTableTime) {
-        this.timeTableTime = timeTableTime;
+    public void setTimeString(String timeString) {
+        this.timeString = timeString;
     }
 
     /**
-     * @return the timeOrderNo
+     * @return the timeOrder
      */
-    public int getTimeOrderNo() {
-        return timeOrderNo;
+    public int getTimeOrder() {
+        return timeOrder;
     }
 
     /**
-     * @param timeOrderNo the timeOrderNo to set
+     * @param timeOrder the timeOrder to set
      */
-    public void setTimeOrderNo(int timeOrderNo) {
-        this.timeOrderNo = timeOrderNo;
+    public void setTimeOrder(int timeOrder) {
+        this.timeOrder = timeOrder;
     }
 
     /**
@@ -133,5 +149,119 @@ public class deliveryPlanDetails {
     public void setDay(String day) {
         this.day = day;
     }
+
+    /**
+     * @return the level
+     */
+    public String getLevel() {
+        return level;
+    }
+
+    /**
+     * @param level the level to set
+     */
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    /**
+     * @return the modueName
+     */
+    public String getModueName() {
+        return modueName;
+    }
+
+    /**
+     * @param modueName the modueName to set
+     */
+    public void setModueName(String modueName) {
+        this.modueName = modueName;
+    }
+
+    /**
+     * @return the moduleCode
+     */
+    public String getModuleCode() {
+        return moduleCode;
+    }
+
+    /**
+     * @param moduleCode the moduleCode to set
+     */
+    public void setModuleCode(String moduleCode) {
+        this.moduleCode = moduleCode;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the lecturerName
+     */
+    public String getLecturerName() {
+        return lecturerName;
+    }
+
+    /**
+     * @param lecturerName the lecturerName to set
+     */
+    public void setLecturerName(String lecturerName) {
+        this.lecturerName = lecturerName;
+    }
+
+    /**
+     * @return the roomName
+     */
+    public String getRoomName() {
+        return roomName;
+    }
+
+    /**
+     * @param roomName the roomName to set
+     */
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    /**
+     * @return the courseName
+     */
+    public String getCourseName() {
+        return courseName;
+    }
+
+    /**
+     * @param courseName the courseName to set
+     */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    /**
+     * @return the groupName
+     */
+    public String getGroupName() {
+        return groupName;
+    }
+
+    /**
+     * @param groupName the groupName to set
+     */
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+    
+    
 
 }
